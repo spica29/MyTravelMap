104069f5aadca208f29c7306a174574130a660f8
thelittlehawk <faik.catibusic@live.com>
2014-12-08 19:10:39 +0100

    GoogleMap

--- MyTravelMap/AppDelegate.m
+++ MyTravelMap/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // GoogleMap
+    [GMSServices provideAPIKey:@""];
     // Override point for customization after application launch.
     return YES;
 }
